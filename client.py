b5336724a2f3c886000f82a4d70372c978c305d8
mephisto-v <agentunknown@infopeklo.cz>
2025-03-08 13:03:57 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-create a remote access tool a client(app) and server (GUI) app named TeamViewer 2 like TeamViewer 1 or QuasarRAT the server will be n GUI                                                               
+create in python a remote access tool a client(app) and server (GUI) app named TeamViewer 2 like TeamViewer 1 or QuasarRAT the server will be n GUI                                                               
  Features: 
 📂 File Manager (Browse files)                                                                                                             🔐Encrypted communication (TLS)
 🎥 Remote Desktop (Screen streaming, Webcam, mouse, and keyboard control)
@@ -7,4 +7,4 @@ create a remote access tool a client(app) and server (GUI) app named TeamViewer 
 🖥️ Remote Shell (Send CLI commands)
 
 👥 Client List (See connected clients)
-Note: The client can't do nothing. make it advanced like QuasarRAT  and like rdp what laguages do you RECOMMEND me
+Note: The client can't do nothing. make it advanced like QuasarRAT  and like rdp run on 0.0.0.0 port 4782
